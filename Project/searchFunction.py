3e4e310fdb4989587699359e47f242ce09cc86e3
namyrrep <141270318+namyrrep@users.noreply.github.com>
2025-09-27 21:50:17 +0000

    Context and making search in python

--- Project/searchFunction.py
+++ Project/searchFunction.py
@@ -1,10 +1,34 @@
 # This is the search functionality implmementation using open ai
 import os
+from dotenv import load_dotenv
 import openai
 
 #checks to see if api key is in environment variables
 if(os.path.exists('.env')):
+    load_dotenv()
     api_key = os.getenv('OPENAI_API_KEY')
     openai.api_key = api_key
 else:
     raise Exception("No .env file found, Please read the README for instructions on how to set up the .env file")
+
+# Context for the model to use when searching
+HOUSING_AGENT_CONTEXT = """
+You are a housing agent that help users find rentals in Blacksburg, VA.
+
+A user will implement a search that has context about what they are looking for in a rental. You need to parse this search and only use information relevant to distance, rent price, number of bedrooms, number of bathrooms, and address.
+If some of this information is missing, you should assume the user is open to any option.
+
+You are not allowed to make up any information about the rentals. If the user asks for information you do not have, you should respond with "I don't know".
+You should only respond with a JSON object with the following format:
+{
+  "address": "string",
+  "distance from drillfield in miles": "integer",
+  "rent_price": "integer",
+  "num_bedrooms": "integer",
+  "num_bathrooms": "integer"
+}
+You are only allowed to respond with this JSON object, and nothing else.
+Only ever respond with 3 rentals at once, if there is less than 3 rentals that match the user's search, you should respond with less than 3 rentals.
+If there are no rentals that match the user's search, you should respond with an empty list.
+"""
+def search_rentals(user_search): #This stores the search function using our context and given user search
